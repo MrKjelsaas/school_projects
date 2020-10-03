c59b61b48c0762dcbc13a28146636295558fa4ee
Magnus Kjelsaas <mr.kjelsaas@gmail.com>
2020-10-03 18:57:38 +0200

    Set firing state

--- ACIT4610/Project/main.py
+++ ACIT4610/Project/main.py
@@ -5,28 +5,37 @@ from random import random
 
 
 # Essential variables
-sampling_rate = 25000
+sampling_rate = 10000
 dt = 1/sampling_rate
 refractory_period = 0
 learning_rate = 0.05  # Number between 0 and 1
-number_of_steps = 30*60*sampling_rate  # Each step is 40us, and each epoke lasts 30 min
+number_of_steps = 30*60*sampling_rate  # Each recording lasts 30 min
 
 # Make a network with 59 nodes
 network = nx.Graph()
-for n in range(59):
-    network.add_node(n+1)
+for n in range(60):
+    network.add_node(n)
 
 # Add weighted edges between all nodes
-for i in range(59):
-    for j in range(i+1, 59):
-        network.add_edge(i+1, j+1, weight=random())
+for i in range(60):
+    for j in range(i+1, 60):
+        network.add_edge(i, j, weight=random())
+
+# Set initial firing state
+nx.set_node_attributes(network, 0, 'Firing')
+print(network.nodes[0])
+# Change firing state
+network.nodes[0]['Firing'] = 1
+print(network.nodes[0])
+network.nodes[0]['Firing'] = 0
+
 
 """
 # Update weights
-print(network[1][59]['weight'])
+print(network[0][59]['weight'])
 for i in range(10):
-    network[1][59]['weight'] += (1-network[1][59]['weight']) * learning_rate
-    print(network[1][59]['weight'])
+    network[0][59]['weight'] += (1-network[0][59]['weight']) * learning_rate
+    print(network[0][59]['weight'])
 """
 
 
@@ -46,23 +55,17 @@ plt.show()
 
 # Import the data
 data = np.loadtxt(r"Data/Dense - 2-1-20.spk.txt")
-print(np.size(data))
-print(np.shape(data))
+#print(np.size(data))
+#print(np.shape(data))
 
 # Count the number of times each neuron fires
-occurences = np.zeros([59])
+times_fired_in_experiment = np.zeros([60])
 for n in range(len(data)):
-    occurences[int(data[n, 1])-1] += 1
-print(occurences)
+    times_fired_in_experiment[int(data[n, 1])-1] += 1
+#print(times_fired_in_experiment)
 
 # Count the average firing rates
-space = 100000
-for n in range(len(data)-1):
-    time = abs(data[n, 0] - data[n+1, 0])
-    if time != 0:
-        if time < space:
-            space = time
-print(time)
+
 
 """
 Slagplan:
